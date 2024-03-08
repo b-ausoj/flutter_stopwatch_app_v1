73e91a22e15dfb0215f529d3805f5f6656b2dadb
b-ausoj <49319850+b-ausoj@users.noreply.github.com>
2024-03-08 10:22:02 +0100

    added comments

--- lib/services/shared_preferences_service.dart
+++ lib/services/shared_preferences_service.dart
@@ -14,9 +14,12 @@ import 'package:flutter_stopwatch_app_v1/widgets/cards/recording_card.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 // This function is only executed once per app lifecycle namely for the initialization
-Future<void> loadData(List<SetupModel> setups, String key) async {
+// and loades all the data i.e. the setups with their stopwatches
+Future<void> _loadData(List<SetupModel> setups, String key) async {
   final prefs = await SharedPreferences.getInstance();
+  // loades the next stopwatch id (so it is unique)
   StopwatchModel.nextId = prefs.getInt("nextStopwatchId") ?? 1;
+  // loades the setups with their stopwatchs
   List<String> jsons = prefs.getStringList(key) ?? [];
   for (String json in jsons) {
     SetupModel loaded = SetupModel.fromJson(jsonDecode(json));
@@ -26,11 +29,14 @@ Future<void> loadData(List<SetupModel> setups, String key) async {
   }
 }
 
+// This function is a wrapper around the loadData (without the release build doesn't work)
 loadStart(StartController startController) async {
-  loadData(startController.allSetups, startController.sharedPreferencesKey)
+  _loadData(startController.allSetups, startController.sharedPreferencesKey)
       .then((value) => startController.refreshBadgeState());
 }
 
+// This function is only executed once per app lifecycle namely for the initialization
+// and loades the settings
 Future<void> loadSettings(SettingsModel settingsModel) async {
   final prefs = await SharedPreferences.getInstance();
   String? json = prefs.getString("settings");
@@ -39,11 +45,15 @@ Future<void> loadSettings(SettingsModel settingsModel) async {
   }
 }
 
+// This function stores the settings into shared preferences
 Future<void> storeSettings(SettingsModel settings) async {
   final prefs = await SharedPreferences.getInstance();
   prefs.setString("settings", jsonEncode(settings));
 }
 
+// This function gets executed everytime a user opens the recordings page
+// and loades all the recordings and then calls the function so it creates
+// the set of cards to be displayed
 Future<void> loadRecordings(
     RecordingsPageController recordingsPageController) async {
   final prefs = await SharedPreferences.getInstance();
@@ -64,6 +74,7 @@ Future<void> loadRecordings(
   recordingsPageController.refresh();
 }
 
+// Only for debugging
 Future<void> logAllSharedPreferences() async {
   final prefs = await SharedPreferences.getInstance();
   log("\nlogAllSharedPreferences\n\n");
@@ -74,12 +85,14 @@ Future<void> logAllSharedPreferences() async {
   log("\nlogAllSharedPreferences end\n\n");
 }
 
+// Only for debugging
 Future<void> resetSharedPreferences() async {
-  // TODO: only for debugging
   final prefs = await SharedPreferences.getInstance();
   prefs.clear();
 }
 
+// This function turns the given stopwatchModel into a recording and then saves 
+// the recording into shared preferences recordings list
 Future<void> saveStopwatch(
     StopwatchModel stopwatchModel, String setupName) async {
   final prefs = await SharedPreferences.getInstance();
@@ -105,10 +118,13 @@ Future<void> saveStopwatch(
   stopwatchModel.reset();
 }
 
-// TODO: Should test this function
+// This function stores the data i.e. next stopwatch id and all the setups
+// with their stopwatchs into shared preferences, can get executed periodically
 Future<void> storeData(List<SetupModel> setups, String key) async {
   final prefs = await SharedPreferences.getInstance();
+  // stores the next stopwatch id (so it is unique)
   prefs.setInt("nextStopwatchId", StopwatchModel.nextId);
+  // stores the setups with their stopwatchs
   List<String> jsons = [];
   for (SetupModel setupModel in setups) {
     jsons.add(jsonEncode(setupModel));
@@ -116,6 +132,9 @@ Future<void> storeData(List<SetupModel> setups, String key) async {
   prefs.setStringList(key, jsons);
 }
 
+// This function stores all the recordings into shared preferences
+// and is used when some recordings got deleted or the were read and some state chages
+// (use when more than one changed or some got deleted)
 Future<void> storeRecordingsState(
     RecordingsPageController recordingsPageController) async {
   final prefs = await SharedPreferences.getInstance();
@@ -126,12 +145,16 @@ Future<void> storeRecordingsState(
   prefs.setStringList("recordings", recordings);
 }
 
+// This function overrides a recording in shared preferences with the given one
+// and is used when a recording got renamed (use when only one recording changes)
 Future<void> storeRecordingState(RecordingModel model) async {
   final prefs = await SharedPreferences.getInstance();
   List<String> recordings = prefs.getStringList("recordings") ?? [];
+  // iterate over the recordings and only the correct one should be removed
   recordings.removeWhere(
     (element) => jsonDecode(element)["id"] == model.id,
   );
+  // and then added the updated one
   recordings.add(jsonEncode(model));
   prefs.setStringList("recordings", recordings);
 }
